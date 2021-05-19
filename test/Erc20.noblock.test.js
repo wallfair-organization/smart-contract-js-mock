93eb6d74356f46e45f19e2bc92315b053b937fb1
Konstantin Ullrich <konstantinullrich12@gmail.com>
2021-05-19 23:52:57 +0200

    Add Jest Tests

---
+++ test/Erc20.noblock.test.js
@@ -0,0 +1,49 @@
+const { setupDatabase, teardownDatabase } = require('../utils/db_helper');
+const ERC20 = require('../erc20_moc/Erc20.noblock');
+
+beforeAll(async () => {
+    return await setupDatabase();
+});
+
+afterAll(async () => {
+    return await teardownDatabase();
+});
+
+test('Mint Tokens', async () => {
+    const tokensToMint = 1000;
+    const testMintWallet = 'testMint';
+
+    const EVNT = new ERC20('EVNT');
+
+    await EVNT.mint(testMintWallet, tokensToMint);
+
+    expect(await EVNT.balanceOf(testMintWallet)).toBe(tokensToMint);
+});
+
+test('Transfer Tokens', async () => {
+    const tokensToMint = 1000;
+    const tokensToTransfer = 100;
+    const testSenderWallet = 'testSender';
+    const testReceiverWallet = 'testReceiver';
+
+    const EVNT = new ERC20('EVNT');
+
+    await EVNT.mint(testSenderWallet, tokensToMint);
+    await EVNT.transfer(testSenderWallet, testReceiverWallet, tokensToTransfer);
+
+    expect(await EVNT.balanceOf(testSenderWallet)).toBe(tokensToMint - tokensToTransfer);
+    expect(await EVNT.balanceOf(testReceiverWallet)).toBe(tokensToTransfer);
+});
+
+test('Burn Tokens', async () => {
+    const tokensToMint = 1000;
+    const tokensToBurn = 100;
+    const testBurnWallet = 'testBurn';
+
+    const EVNT = new ERC20('EVNT');
+
+    await EVNT.mint(testBurnWallet, tokensToMint);
+    await EVNT.burn(testBurnWallet, tokensToBurn);
+
+    expect(await EVNT.balanceOf(testBurnWallet)).toBe(tokensToMint - tokensToBurn);
+});
