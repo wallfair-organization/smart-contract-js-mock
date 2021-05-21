0f6f1a48b7b36322ca7bf50eb4c11b34a01d14cc
Konstantin Ullrich <konstantinullrich12@gmail.com>
2021-05-21 18:20:36 +0200

    Improved Lib

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-const ERC20 = require('./erc20_moc/Erc20.noblock');
+const Erc20 = require('./erc20_moc/Erc20.noblock');
 const Bet = require('./erc20_moc/Bet.noblock');
+const NoWeb3Exception = require('./erc20_moc/Exception.noblock');
 
-async function main() {
-    const erc20 = new ERC20("EVNT");
-    await erc20.mint("konsti", 100000000)
-
-    const bet = new Bet("konstanntin_bet2");
-    await bet.addLiquidity('konsti', 1000);
-    console.log(await bet.isResolved());
-
-    console.log("🚀 Buy for 100 EVNT:\t" + await bet.calcBuy(100, "yes") + " YesToken");
-    console.log("🚀 Buy for 100 EVNT:\t" + await bet.calcBuy(100, "no") + " NoToken");
-
-    try {
-        await bet.buy("konsti", 100, "yes", 0);
-    } catch (e) {
-        console.log(e.message);
-    }
-
-    console.log("🚀 Buy for 100 EVNT:\t" + await bet.calcBuy(100, "yes") + " YesToken");
-    console.log("🚀 Buy for 100 EVNT:\t" + await bet.calcBuy(100, "no") + " NoToken");
+module.exports = {
+    Erc20,
+    Bet,
+    NoWeb3Exception
 }
-
-main().then(() => process.exit());
