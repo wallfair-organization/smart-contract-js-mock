5bee8cf29874fcf6f76e752635cd5e192c35f9d7
Konstantin Ullrich <konstantinullrich12@gmail.com>
2021-05-21 21:18:44 +0200

    Improved Lib

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Erc20 = require('./erc20_moc/Erc20.noblock');
-const Bet = require('./erc20_moc/Bet.noblock');
+const BetContract = require('./erc20_moc/Bet.noblock');
 const NoWeb3Exception = require('./erc20_moc/Exception.noblock');
 
 module.exports = {
     Erc20,
-    Bet,
+    BetContract,
     NoWeb3Exception
 }
