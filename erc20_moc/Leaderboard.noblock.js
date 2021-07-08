6943eb97f7abd7ca09c97a1cdc0c2a0fed3bb56f
Konsti <konsti@wallfair.io>
2021-07-08 11:49:42 +0200

    Update JSDoc and sort leaderboard

--- erc20_moc/Leaderboard.noblock.js
+++ erc20_moc/Leaderboard.noblock.js
@@ -1,10 +1,21 @@
 const {viewLimitBalancesOfToken} = require('../utils/db_helper');
 
 class Leaderboard {
+    /**
+     * Create a Moc Leaderboard instance
+     *
+     * @param symbol {string}
+     */
     constructor(symbol) {
         this.symbol = symbol;
     }
 
+    /**
+     * Get a limited list of the leaders ordered by their EVNT Balance
+     *
+     * @param limit {number}
+     * @returns {Promise<*>}
+     */
     getLeaders = async (limit) => await viewLimitBalancesOfToken(this.symbol, limit);
 }
 
