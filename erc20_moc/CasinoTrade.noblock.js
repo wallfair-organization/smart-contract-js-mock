c4b162c683569c0e982249a339c9b87c1ed6a47b
Guilherme Mussi <gmussi@gmail.com>
2021-08-23 22:52:44 +0200

    add casino trading functionality

---
+++ erc20_moc/CasinoTrade.noblock.js
@@ -0,0 +1,74 @@
+const ERC20 = require('./Erc20.noblock');
+const NoWeb3Exception = require('./Exception.noblock');
+const bigDecimal = require("js-big-decimal");
+
+const {
+    CASINO_TRADE_STATE,
+    createDBTransaction,
+    rollbackDBTransaction,
+    commitDBTransaction,
+    insertCasinoTrade,
+    lockOpenCasinoTrades,
+    setCasinoTradeOutcomes,
+    getCasinoTrades
+} = require('../utils/db_helper');
+
+const EVNT_TOKEN = 'EVNT';
+
+class CasinoTrade {
+    constructor(casinoWalletAddr) {
+        this.casinoWalletAddr = casinoWalletAddr;
+        this.CASINO_TRADE_STATE = CASINO_TRADE_STATE;
+        this.EVNTToken = new ERC20(EVNT_TOKEN);
+    }
+
+    placeTrade = async (userWalletAddr, stakedAmount, crashFactor) => {
+        const dbClient = await createDBTransaction();
+
+        try {
+            // in the same transaction, transfer the funds, and create the casino trade
+            await this.EVNTToken.transferChain(dbClient, userWalletAddr, this.casinoWalletAddr, stakedAmount);
+            await insertCasinoTrade(dbClient, userWalletAddr, crashFactor, stakedAmount);
+
+            await commitDBTransaction(dbClient);
+        } catch (e) {
+            await rollbackDBTransaction(dbClient);
+            throw e;
+        }
+    }
+
+    lockOpenTrades = async (gameId) => {
+        const dbClient = await createDBTransaction();
+
+        try {
+            await lockOpenCasinoTrades(dbClient, gameId);
+
+            await commitDBTransaction(dbClient);
+        } catch (e) {
+            await rollbackDBTransaction(dbClient);
+            throw e;
+        }
+    }
+
+    rewardWinners = async (gameId, decidedCrashFactor) => {
+        const dbClient = await createDBTransaction();
+
+        try {
+            await setCasinoTradeOutcomes(dbClient, gameId, decidedCrashFactor);
+            let winners = await getCasinoTrades(dbClient, gameId, CASINO_TRADE_STATE.WIN);
+            for (let winner of winners) {
+                let reward = bigDecimal.multiply(BigInt(winner.stakedamount), decidedCrashFactor);
+                reward = BigInt(bigDecimal.round(reward));
+
+                await this.EVNTToken.transferChain(dbClient, this.casinoWalletAddr, winner.userid, reward);
+            }
+
+            await commitDBTransaction(dbClient);
+        } catch (e) {
+            await rollbackDBTransaction(dbClient);
+            throw e;
+        }
+    }
+}
+
+module.exports = CasinoTrade;
