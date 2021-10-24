0e00a2fc0b65e88769cd58ae5d488a427566eebf
Guilherme Mussi <gmussi@gmail.com>
2021-10-24 13:26:56 +0200

    rename gameId to gameHash

--- erc20_moc/CasinoTrade.noblock.js
+++ erc20_moc/CasinoTrade.noblock.js
@@ -56,11 +56,11 @@ class CasinoTrade {
     }
   };
 
-  cashout = async (userWalletAddr, crashFactor, gameId) => {
+  cashout = async (userWalletAddr, crashFactor, gameHash) => {
     const dbClient = await createDBTransaction();
 
     try {
-      let res = await attemptCashout(dbClient, userWalletAddr, gameId, crashFactor);
+      let res = await attemptCashout(dbClient, userWalletAddr, gameHash, crashFactor);
 
       if (res.rows.length == 0) {
         throw 'Transaction did not succeed';
@@ -98,15 +98,15 @@ class CasinoTrade {
     }
   };
 
-  rewardWinners = async (gameId, decidedCrashFactor) => {
+  rewardWinners = async (gameHash, decidedCrashFactor) => {
     const dbClient = await createDBTransaction();
 
     try {
-      let result = await setCasinoTradeOutcomes(dbClient, gameId, decidedCrashFactor);
+      let result = await setCasinoTradeOutcomes(dbClient, gameHash, decidedCrashFactor);
       let winners = result.rows;
       /*let winners = await getCasinoTrades(
         dbClient,
-        gameId,
+        gameHash,
         CASINO_TRADE_STATE.WIN
       );*/
       for (let winner of winners) {
